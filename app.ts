359b9f538560a8765d5bb9a14bc3ad317e2ae363
athicsmsu <65011212145@msu.ac.th>
2024-03-07 22:11:42 +0700

    commit

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { router as movie } from "./api/movie";
 import { router as person } from "./api/person";
 import { router as star } from "./api/star";
+import { router as creator } from "./api/creator";
 
 import bodyParser from "body-parser";
 
@@ -19,3 +20,4 @@ app.use(bodyParser.json());
 app.use("/movie", movie);
 app.use("/person", person);
 app.use("/star", star);
+app.use("/creator", creator);
